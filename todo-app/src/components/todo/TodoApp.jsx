4c81de261cbedc5b04447165cc2919bc77a13539
Phlipr <preynolds1406@gmail.com>
2020-12-03 21:15:00 -0600

    Added Header and Footer components along with bootstrap.css.

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+
 import Login from './Login'
 import Welcome from './Welcome'
 import Error from './Error'
 import TodoList from './TodoList'
+import HeaderComponent from './Header'
+import FooterComponent from './Footer'
+
+
+import './bootstrap.css'
 
 class TodoApp extends Component {
     render() {
         return (
             <Router>
+                <HeaderComponent/>
                 <Switch>
                     <Route path='/' exact component={Login}/>
+                    <Route path='/login' component={Login}/>
                     <Route path='/welcome/:name' component={Welcome}/>
                     <Route path='/todolists' component={TodoList}/>
                     <Route path='' component={Error}/>
                 </Switch>
+                <FooterComponent/>
             </Router>
         )
     }
