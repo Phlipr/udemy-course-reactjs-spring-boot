394bd6a642e32550dfd8c4d0d602ebd4b43568aa
Phlipr <preynolds1406@gmail.com>
2020-12-12 19:07:27 -0600

    Added simple Hello World controller to display Hello Wolrd when URI /hello-world was used.

---
+++ restful-web-services/src/main/java/com/PWRedApplications/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.PWRedApplications.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+	//GET
+	//URI - /hello-world	
+	//method - "Hello World"
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
