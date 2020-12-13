8234646c2e43e2d7cae46a8f66257b56c6d1ec39
Phlipr <preynolds1406@gmail.com>
2020-12-12 19:41:20 -0600

    Added Bean to create JSON object and added method to take path variable and incorporate it into the JSON object.

--- restful-web-services/src/main/java/com/PWRedApplications/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/PWRedApplications/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,16 +1,25 @@
 package com.PWRedApplications.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
 public class HelloWorldController {
-	//GET
-	//URI - /hello-world	
-	//method - "Hello World"
+	
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldBean(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s!", name));
 	}
 }
