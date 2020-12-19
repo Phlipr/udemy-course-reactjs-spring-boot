d091848602e5cef490c8f839ecf3dba3d629ad32
Phlipr <preynolds1406@gmail.com>
2020-12-18 19:41:14 -0600

    Added form to TodoComponent using formik dependency.

--- todo-app/src/components/todo/TodoComponent.jsx
+++ todo-app/src/components/todo/TodoComponent.jsx
@@ -1,9 +1,43 @@
 import React, { Component } from 'react'
+import moment from 'moment'
+import { Field, Form, Formik } from 'formik'
 
 class TodoComponent extends Component {
+    
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id : this.props.match.params.id,
+            description : 'Learn forms',
+            targetDate : moment(new Date()).format('YYYY-MM-DD')
+        }
+    }
+    
     render() {
         return (
-            <div>Todo # {this.props.match.params.id}</div>
+            <div>
+                <h1>Todo #{this.props.match.params.id}</h1>
+                <div className="container">
+                    <Formik>
+                        {
+                            (props) => (
+                                <Form>
+                                    <fieldset className="form-group">
+                                        <label>Description</label>
+                                        <Field className="form-control" type="text" name="description"/>
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <label>Target Date</label>
+                                        <Field className="form-control" type="date" name="targetDate"/>
+                                    </fieldset>
+                                    <button className="btn btn-success" type="Submit">Save</button>
+                                </Form>
+                            )
+                        }
+                    </Formik>
+                </div>
+            </div>
         )
     }
 }
