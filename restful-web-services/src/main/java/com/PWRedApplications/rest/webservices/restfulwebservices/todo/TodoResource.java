462a276aa9bea775d251286e1955f3d6ab053c92
Phlipr <preynolds1406@gmail.com>
2020-12-18 19:17:35 -0600

    Removed unused import of Todo from TodoService in API.

--- restful-web-services/src/main/java/com/PWRedApplications/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ restful-web-services/src/main/java/com/PWRedApplications/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.PWRedApplications.rest.webservices.restfulwebservices.todo.Todo;
-
 @RestController
 @CrossOrigin(origins="http://localhost:4200")
 public class TodoResource {
