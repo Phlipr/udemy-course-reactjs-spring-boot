08619a93b1da8d262b65a30be06b3ba9b2b147fa
Phlipr <preynolds1406@gmail.com>
2021-01-17 20:44:33 -0600

    Added BasicAuthenticationService to API backend.

---
+++ restful-web-services/src/main/java/com/PWRedApplications/rest/webservices/restfulwebservices/basic/auth/AuthenticationBean.java
@@ -0,0 +1,26 @@
+package com.PWRedApplications.rest.webservices.restfulwebservices.basic.auth;
+
+public class AuthenticationBean {
+
+	private String message;
+
+	public AuthenticationBean(String message) {
+		this.message = message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	
+	
+	
+}
