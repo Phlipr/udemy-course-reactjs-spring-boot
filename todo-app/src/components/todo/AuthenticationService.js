ef8815b669fb0c77e750c872826b5983119ead00
Phlipr <preynolds1406@gmail.com>
2021-01-24 19:06:56 -0600

    Added constant for authenticatedUser field in AuthenticationService.

--- todo-app/src/components/todo/AuthenticationService.js
+++ todo-app/src/components/todo/AuthenticationService.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+export const USER_NAME_SESSION_ATTRIBUTE_NAME = 'authenticatedUser'
+
 class AuthenticationService {
 
     executeBasicAuthenticationService(username, password) {
@@ -20,12 +22,12 @@ class AuthenticationService {
     }
 
     registerSuccessfulLogin(username, password) {
-        sessionStorage.setItem('authenticatedUser', username);
+        sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username);
         this.setupAxiosInterceptors(this.createBasicAuthToken(username, password));
     }
 
     registerSuccessfulJwtLogin(username, token) {
-        sessionStorage.setItem('authenticatedUser', username);
+        sessionStorage.setItem(USER_NAME_SESSION_ATTRIBUTE_NAME, username);
         this.setupAxiosInterceptors(this.createJwtAuthToken(token));
     }
 
@@ -34,11 +36,11 @@ class AuthenticationService {
     }
 
     logout() {
-        sessionStorage.removeItem('authenticatedUser');
+        sessionStorage.removeItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
     }
 
     isUserLoggedIn() {
-        let user = sessionStorage.getItem('authenticatedUser');
+        let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
 
         if (user == null) return false;
 
@@ -46,7 +48,7 @@ class AuthenticationService {
     }
 
     getLoggedInUserName() {
-        let user = sessionStorage.getItem('authenticatedUser');
+        let user = sessionStorage.getItem(USER_NAME_SESSION_ATTRIBUTE_NAME);
         if (user===null) return '';
         return user;
     }
